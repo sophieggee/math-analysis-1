7aec12564258ae446fc83e30f6cd9d4281e5a993
sophie gee <sophieggee@icloud.com>
2021-09-13 17:12:39 -0600

    pushing lab 1

--- MatplotlibIntro/matplotlib_intro.py
+++ MatplotlibIntro/matplotlib_intro.py
@@ -1,10 +1,11 @@
 # matplotlib_intro.py
 """Python Essentials: Intro to Matplotlib.
-<Name>
-<Class>
-<Date>
+<Sophie Gee>
+<section 3>
+<09/12/21>
 """
-
+import numpy as np
+from matplotlib import pyplot as plt
 
 # Problem 1
 def var_of_means(n):
@@ -18,13 +19,21 @@ def var_of_means(n):
     Returns:
         (float) The variance of the means of each row.
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+    matrix_n = np.random.normal(size=(n,n)) #random matrix A with values drawn from the standard normal distribution
+    mean = np.mean(matrix_n, axis=1) #mean value of each row
+    variance = np.var(mean) #variance of these means
+    return variance
 
 def prob1():
     """Create an array of the results of var_of_means() with inputs
     n = 100, 200, ..., 1000. Plot and show the resulting array.
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+    array = [] #create array for results
+    for n in range(100, 1001, 100):
+        array += [var_of_means(n)] #find and insert results
+    x = np.linspace(100, 1000, 10) #create x axis
+    plt.plot(x,array) #plot
+    plt.show() #show the plot
 
 
 # Problem 2
@@ -33,7 +42,16 @@ def prob2():
     [-2pi, 2pi]. Make sure the domain is refined enough to produce a figure
     with good resolution.
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+    neg_2_pi = -2*np.pi
+    pos_2_pi = 2*np.pi
+
+    x = np.linspace(neg_2_pi, pos_2_pi, 100) #creating a suitable domain
+    y_sin = np.sin(x)
+    y_cos = np.cos(x)
+    y_arctan = np.arctan(x)
+    plt.plot(x,y_sin) #plotting sin
+    plt.plot(x,y_cos) #plotting cos
+    plt.plot(x,y_arctan) #plotting arctan
 
 
 # Problem 3
@@ -44,8 +62,15 @@ def prob3():
         3. Set the range of the x-axis to [-2,6] and the range of the
            y-axis to [-6,6].
     """
-    raise NotImplementedError("Problem 3 Incomplete")
-
+    x1 = np.linspace(-2,1,100, endpoint=False) #?????
+    y1 = 1/(x1-1)
+    plt.plot(x1,y1, 'm--', linewidth=4)
+    x2 = np.linspace(1.01,6,100) #????
+    y2 = 1/(x2-1)
+    plt.plot(x2,y2, 'm--', linewidth=4)
+    plt.xlim(-2,6)
+    plt.ylim(-6,6)
+    plt.show()
 
 # Problem 4
 def prob4():
@@ -61,7 +86,35 @@ def prob4():
              2sin(x): blue dashed line.
             2sin(2x): magenta dotted line.
     """
-    raise NotImplementedError("Problem 4 Incomplete")
+    ax1 = plt.subplot(221) #setting to top right corrner
+    ax1.axis([0, 2*np.pi, -2, 2])
+    x1 = np.linspace(0, 2*np.pi, 100) #creating a suitable domain
+    y1_sin = np.sin(x1) 
+    ax1.plot(x1, y1_sin, 'g')
+    ax1.set_title("sin(x)", fontsize=10)
+    
+    ax2 = plt.subplot(222) #setting to top left corner
+    ax2.axis([0, 2*np.pi, -2, 2])
+    x2 = np.linspace(0, 2*np.pi, 100) #creating a suitable domain
+    y2_sin = np.sin(2*x2)  #sin(2x)
+    ax2.plot(x2, y2_sin, 'r--')
+    ax2.set_title("sin(2x)", fontsize=10) #setting title
+    
+    ax3 = plt.subplot(223) #setting to bottom right corner
+    ax3.axis([0, 2*np.pi, -2, 2])
+    x3 = np.linspace(0, 2*np.pi, 100) #creating a suitable domain
+    y3_sin = 2*np.sin(x3)  #2sin(x)
+    ax3.plot(x3, y3_sin, 'b--')
+    ax3.set_title("2sin(x)", fontsize=10) #setting title
+    
+    ax4 = plt.subplot(224) #setting to bottom left corner
+    ax4.axis([0, 2*np.pi, -2, 2])
+    x4 = np.linspace(0, 2*np.pi, 100) #creating a suitable domain
+    y4_sin = 2*np.sin(2*x4)  #2sin(2x)
+    ax4.plot(x4, y4_sin, 'm:')
+    ax4.set_title("2sin(2x)", fontsize=10) #setting title
+    plt.suptitle("Sin Graphs", fontsize = 15) #setting overall title
+    plt.show()
 
 
 # Problem 5
@@ -74,7 +127,7 @@ def prob5():
         2. A histogram of the hours of the day, with one bin per hour.
             Label and set the limits of the x-axis.
     """
-    raise NotImplementedError("Problem 5 Incomplete")
+    car_crash_data = np.load('FARS.npy')
 
 
 # Problem 6
