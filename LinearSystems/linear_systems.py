1a8e8cfe42c32d51f05348e4a111c35b8df2809a
sophie gee <sophieggee@icloud.com>
2021-09-28 11:55:28 -0600

    pushing lab 5

--- LinearSystems/linear_systems.py
+++ LinearSystems/linear_systems.py
@@ -1,10 +1,15 @@
 # linear_systems.py
 """Volume 1: Linear Systems.
-<Name>
-<Class>
-<Date>
+<Sophie Gee>
+<section 3>
+<9.28>
 """
-
+import numpy as np
+from scipy import linalg as la
+import time
+from matplotlib import pyplot as plt
+from scipy import sparse
+from scipy.sparse import linalg as spla
 
 # Problem 1
 def ref(A):
@@ -18,7 +23,15 @@ def ref(A):
     Returns:
         ((n,n) ndarray): The REF of A.
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+    n = A.shape[0]
+
+    #row reduction
+    for j in range(n-1):            #iterates through rows until last row
+        for i in range(j+1, n):   #iterates through rows after i
+            c = A[i,j]/A[j,j]       #finds scalar for multiplication
+            A[i,j+1:n] = A[i,j+1:n] - c*A[j,j+1:n]  #subtract the scalar multiple of the upper row from the lower row
+            A[i,j] = 0
+    return A
 
 
 # Problem 2
@@ -33,8 +46,14 @@ def lu(A):
         L ((n,n) ndarray): The lower-triangular part of the decomposition.
         U ((n,n) ndarray): The upper-triangular part of the decomposition.
     """
-    raise NotImplementedError("Problem 2 Incomplete")
-
+    n = A.shape[0] #returns dimension n of square matrix A
+    U = A.copy() #creates copy of A
+    L = np.identity(n) #identity of size n 
+    for j in range(n): # for j= 0,..,n-1
+        for i in range(j+1, n): # for i= j+1,...,n-1
+            L[i,j] = U[i,j]/U[j,j] #divides rows in U for lower triangular matrix
+            U[i,j:] = U[i,j:] - L[i,j]*U[j,j:] #sets U to be row reduced
+    return L,U
 
 # Problem 3
 def solve(A, b):
@@ -48,7 +67,25 @@ def solve(A, b):
     Returns:
         x ((m,) ndarray): The solution to the linear system.
     """
-    raise NotImplementedError("Problem 3 Incomplete")
+    n = A.shape[0]
+    L,U = lu(A)
+    y = np.zeros((n))
+    x = np.zeros((n))
+    sum = 0
+
+    for k in range(n): #finds sum for each inidvidual y_k
+        for j in range(k):
+            sum += L[k,j]*y[j]
+        y[k] = b[k] - sum #returns new y
+
+    sum2=0    
+    for k in range(n-1, -1, -1): #finds sum for each individual x_k using y_k
+        for j in range(k, n):
+            sum2 += U[k,j]*x[j]
+        x[k] = 1/U[k,k]*((y[k]) - sum2) #returns new x
+    
+    return x
+
 
 
 # Problem 4
@@ -69,7 +106,41 @@ def prob4():
     Plot the system size n versus the execution times. Use log scales if
     needed.
     """
-    raise NotImplementedError("Problem 4 Incomplete")
+    
+    domain = 2**np.arange(1,13)
+    times =[]
+    times2=[]
+    times3=[]
+    times4=[]
+    for n in domain:
+        A = np.random.random((n,n))
+        b = np.random.random(n)
+        start = time.time()
+        np.matmul(la.inv(A),b)
+        times.append(time.time() - start)
+
+        start2 = time.time()
+        la.solve(A,b)
+        times2.append(time.time() - start2)
+
+        start3 = time.time()
+        L, P = la.lu_factor(A)
+        la.lu_solve((L,P), b)
+        times3.append(time.time() - start3)
+
+        L, P = la.lu_factor(A)
+        start4 = time.time()
+        la.lu_solve((L,P), b)
+        times4.append(time.time() - start4)
+
+    plt.plot(domain, times, '.-', linewidth=2, markersize=15, label="invert then left-multiply")
+    plt.plot(domain, times2, '.-', linewidth=2, markersize=15, label="using linalg.solve")
+    plt.plot(domain, times3, '.-', linewidth=2, markersize=15, label="using linalg.lu_solve and linalg.lu_factor")
+    plt.plot(domain, times4, '.-', linewidth=2, markersize=15, label="using linalg.lu_solve")
+    plt.legend()
+    plt.ylabel("time taken to compute")
+    plt.xlabel("matrices of size nxn")
+    plt.show()
 
 
 # Problem 5
@@ -89,7 +160,11 @@ def prob5(n):
     Returns:
         A ((n**2,n**2) SciPy sparse matrix)
     """
-    raise NotImplementedError("Problem 5 Incomplete")
+    B = sparse.diags([1,-4,1],[-1,0,1],shape=(n,n)) #create B
+    A = sparse.block_diag([B]*n) #set A diagonals to B matrices
+    A.setdiag(np.ones(n**2), n) #set other diagonals to identity matrices of size n
+    A.setdiag(np.ones(n**2), -n)
+    return A
 
 
 # Problem 6
@@ -108,4 +183,29 @@ def prob6():
     size n**2 versus the execution times. As always, use log scales where
     appropriate and use a legend to label each line.
     """
-    raise NotImplementedError("Problem 6 Incomplete")
+    domain = 2**np.arange(1,8)
+    times =[]
+    times2=[]
+    for n in domain:
+        A = prob5(n)
+        b = np.random.random(n**2)
+        Acsr = A.tocsr()
+        start = time.time()
+        spla.spsolve(Acsr, b)
+        times.append(time.time() - start)
+
+        A = Acsr.toarray()
+        start2 = time.time()
+        la.solve(A,b)
+        times2.append(time.time() - start2)
+
+    plt.plot(domain, times, '.-', linewidth=2, markersize=15, label="using csr spsolve")
+    plt.plot(domain, times2, '.-', linewidth=2, markersize=15, label="using linalg.solve")
+    plt.ylabel("time taken to compute")
+    plt.xlabel("matrices of size n**2")
+
+    plt.legend()
+    plt.show()
+
+if __name__ == "__main__":
+    prob6()
