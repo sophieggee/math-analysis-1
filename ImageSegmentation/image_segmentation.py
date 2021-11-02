af62e4106d0fc0922e66a180693c7fc1e0063f94
sophie gee <sophieggee@icloud.com>
2021-11-02 11:44:20 -0600

    push

--- ImageSegmentation/image_segmentation.py
+++ ImageSegmentation/image_segmentation.py
@@ -1,12 +1,17 @@
 # image_segmentation.py
 """Volume 1: Image Segmentation.
-<Name>
-<Class>
-<Date>
+<Sophie Gee>
+<section 3>
+<11/2/21>
 """
 
 import numpy as np
-
+from numpy.core.fromnumeric import partition
+from scipy import linalg as la
+from imageio import imread
+from matplotlib import pyplot as plt
+from scipy import sparse
+from scipy.sparse.linalg import eigsh
 
 # Problem 1
 def laplacian(A):
@@ -18,8 +23,12 @@ def laplacian(A):
     Returns:
         L ((N,N) ndarray): The Laplacian matrix of G.
     """
-    raise NotImplementedError("Problem 1 Incomplete")
-
+    n,m = np.shape(A)
+    D = np.zeros((n,n))
+    for i in range(n):
+        D[i,i] = np.sum(A, axis=0)[i]
+    L = D-A
+    return L
 
 # Problem 2
 def connectivity(A, tol=1e-8):
@@ -35,7 +44,14 @@ def connectivity(A, tol=1e-8):
         (int): The number of connected components in G.
         (float): the algebraic connectivity of G.
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+    L = laplacian(A)
+    connect = 0
+    eigs = np.real(la.eigvals(L))
+    for eig in eigs:
+        if abs(eig) < tol:
+            connect += 1
+            eig = 0
+    return (connect, partition(eigs, 1)[1])
 
 
 # Helper function for problem 4.
@@ -78,31 +94,110 @@ class ImageSegmenter:
     # Problem 3
     def __init__(self, filename):
         """Read the image file. Store its brightness values as a flat array."""
-        raise NotImplementedError("Problem 3 Incomplete")
+        image = imread(filename)
+        scaled = image/255
+        self.im=scaled
+
+        #if image is in color
+        if len(np.shape(image)) == 3:
+            brightness = np.mean(scaled,axis=2)
+
+        #else, image is grayscale
+        else:
+            brightness = scaled
+
+        #set brightness, then flatten it
+        self.bright = brightness
+        self.flat_bright = np.ravel(brightness)
+
 
     # Problem 3
     def show_original(self):
         """Display the original image."""
-        raise NotImplementedError("Problem 3 Incomplete")
+        if np.shape(self.im) == 3:
+            plt.imshow(self.im)
+        else:
+            plt.imshow(self.im, cmap="gray")
+        plt.axis("off")
 
     # Problem 4
     def adjacency(self, r=5., sigma_B2=.02, sigma_X2=3.):
         """Compute the Adjacency and Degree matrices for the image graph."""
-        raise NotImplementedError("Problem 4 Incomplete")
+
+        #intitialize variables and set to empty matrices/arrays
+        mn = len(self.flat_bright)
+        A = sparse.lil_matrix((mn,mn))
+        D = np.zeros(mn)
+        B = self.flat_bright
+        if len(np.shape(self.im)) == 3:
+            m,n, _ = np.shape(self.im)
+        else:
+            m,n =np.shape(self.im)
+
+        #go through each index of each pixel in the image
+        for i in range(mn):
+            #get array of Ji and distances between each i and j
+            arr, dist = (get_neighbors(i, r, m, n))
+            b = -abs((B[i]-B[arr]))/sigma_B2
+            x = -(dist/sigma_X2)
+            #set each weight array using the equation given
+            weight = np.exp(b+x)
+            #set A and D
+            A[i, arr]= weight
+            D[i] = np.sum(weight)
+        
+        #convert to diagonal matrix and sparse optimized matrix
+        A = sparse.csc_matrix(A)
+
+        return A,D
 
     # Problem 5
     def cut(self, A, D):
         """Compute the boolean mask that segments the image."""
-        raise NotImplementedError("Problem 5 Incomplete")
+        L = sparse.csgraph.laplacian(A)
+        D1 = sparse.diags([1/np.sqrt(d) for d in D])
+        DLD = D1@L@D1
+        eval, evec = eigsh(DLD,which="SM", k=2)
+        evec = evec[:,1]
+        if len(np.shape(self.im)) == 3:
+            m,n, _ = np.shape(self.im)
+        else:
+            m,n =  np.shape(self.im)
+        reshaped = evec.reshape(m,n)
+        mask = reshaped > 0
+        return mask
 
     # Problem 6
     def segment(self, r=5., sigma_B=.02, sigma_X=3.):
         """Display the original image and its segments."""
-        raise NotImplementedError("Problem 6 Incomplete")
+        A, D = self.adjacency(r, sigma_B, sigma_X)
+        mask = self.cut(A,D)
+        if len(np.shape(self.im)) == 3:
+            if np.shape(self.im)[2] > 3:
+                mask = np.dstack((mask,mask,mask,mask))
+            else:
+                mask = np.dstack((mask,mask,mask))
+    
+
+        plt.subplot(131)
+        plt.imshow(self.im)
+        plt.axis("off")
+
+        pos = self.im*mask
+        plt.subplot(132)
+        plt.imshow(pos)
+        plt.axis("off")
+
+        neg = self.im*~mask
+        plt.subplot(133)
+        plt.imshow(neg)
+
+        plt.axis("off")
+
+        plt.show()
 
 
-# if __name__ == '__main__':
-#     ImageSegmenter("dream_gray.png").segment()
-#     ImageSegmenter("dream.png").segment()
-#     ImageSegmenter("monument_gray.png").segment()
-#     ImageSegmenter("monument.png").segment()
+if __name__ == '__main__':
+    ImageSegmenter("dream_gray.png").segment()
+    ImageSegmenter("dream.png").segment()
+    ImageSegmenter("blue_heart.png").segment()
