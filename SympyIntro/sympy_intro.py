d9c420057fb00055ca77927b2cb046e6759da317
sophie gee <sophieggee@icloud.com>
2022-01-11 10:54:33 -0700

    pushing changes

--- SympyIntro/sympy_intro.py
+++ SympyIntro/sympy_intro.py
@@ -1,10 +1,13 @@
 # sympy_intro.py
 """Python Essentials: Introduction to SymPy.
-<Name>
-<Class>
-<Date>
+<Sophie Gee>
+<lab vol 1>
+<1/11/22>
 """
-
+import numpy as np
+import sympy as sy
+from matplotlib import pyplot as plt
+from sympy import Matrix
 
 # Problem 1
 def prob1():
@@ -14,7 +17,14 @@ def prob1():
 
     Make sure that the fractions remain symbolic.
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+
+    #define variables x and y
+    x = sy.symbols('x')
+    y = sy.symbols('y')
+
+    #establish expression and return
+    expression = sy.Rational(2, 5)*sy.exp(x**2-y)*sy.cosh(x+y)+sy.Rational(3, 7)*sy.log((x*y)+1)
+    return expression
 
 
 # Problem 2
@@ -23,7 +33,12 @@ def prob2():
 
         product_(i=1 to 5)[ sum_(j=i to 5)[j(sin(x) + cos(x))] ]
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+    #establish initial expression
+    x, i, j = sy.symbols('x i j')
+    expr = sy.product(sy.summation(j*(sy.sin(x)+sy.cos(x)),(j, i, 5)), (i,1,5))
+
+    #simplify
+    return sy.simplify(expr)
 
 
 # Problem 3
@@ -33,7 +48,23 @@ def prob3(N):
     Lambdify the resulting expression and plot the series on the domain
     y in [-3,3]. Plot e^(-y^2) over the same domain for comparison.
     """
-    raise NotImplementedError("Problem 3 Incomplete")
+    #define initial expression
+    x, n, y = sy.symbols('x n y')
+    expr = sy.summation((x**n)/sy.factorial(n),(n, 0, N))
+
+    #substitu=te in -y**2
+    y_expr = expr.subs(x, -y**2)
+
+    #set domain, define functions, plot
+    domain = np.linspace(-2,2,1000)
+    f = sy.lambdify(y, y_expr, "numpy")
+    h = lambda x: np.exp(-x**2)
+
+    plt.plot(f(domain),domain, label= "lambdify")
+    plt.plot(h(domain), domain, label= "lambda")
+    plt.title("Lambdifying two different ways")
+    plt.legend()
+    plt.show()
 
 
 # Problem 4
@@ -46,8 +77,26 @@ def prob4():
     it to polar coordinates. Simplify the result, then solve it for r.
     Lambdify a solution and use it to plot x against y for theta in [0, 2pi].
     """
-    raise NotImplementedError("Problem 4 Incomplete")
+    #construct equation 2.2
+    x, y, r, t = sy.symbols('x y r t')
+    expr = 1-((x**2+y**2)**sy.Rational(7,2) + 18*x**5*y - 60*x**3*y**3  +  18*x*y**5)/ (x**2+y**2)**3
 
+    #convert it to polar coordinates
+    pol_expr = expr.subs({x:r*sy.cos(t), y:r*sy.sin(t)})
+
+    #simplify result
+    simp = sy.simplify(pol_expr)
+
+    #solve it for r
+    solutions = sy.solve(simp, r)
+
+    #lambdify solution
+    f = sy.lambdify(t,solutions[1], "numpy")
+
+    domain = np.linspace(0,np.pi*2, 1000)
+    plt.plot(f(domain)*np.cos(domain), f(domain)*np.sin(domain))
+    plt.title("rose curve")
+    plt.show()
 
 # Problem 5
 def prob5():
@@ -61,7 +110,33 @@ def prob5():
         (dict): a dictionary mapping eigenvalues (as expressions) to the
             corresponding eigenvectors (as SymPy matrices).
     """
-    raise NotImplementedError("Problem 5 Incomplete")
+    x, y, l = sy.symbols('x y l')
+
+    #define matrix A, L, and compute eigvals
+    A = sy.Matrix([ [x-y, x, 0],
+                    [x, x-y, x],
+                    [0, x, x-y]])
+
+    L = sy.Matrix([ [l, 0, 0],
+                    [0, l, 0],
+                    [0, 0, l]])
+
+    I = sy.Matrix([ [1, 0, 0],
+                    [0, 1, 0],
+                    [0, 0, 1]])
+    
+    eigvals = sy.solve(sy.det(A-L), l)
+
+
+    dict_of_eigs = {}
+    #compute eigvectors
+    for i in eigvals:
+        eigvect = (A-I*i).nullspace()[0]
+        dict_of_eigs[i] = eigvect
+
+    return dict_of_eigs
+
+
 
 
 # Problem 6
@@ -77,7 +152,42 @@ def prob6():
         (set): the local minima.
         (set): the local maxima.
     """
-    raise NotImplementedError("Problem 6 Incomplete")
+    #create f(x)
+    x = sy.symbols('x')
+
+    f = 2*x**6 - 51*x**4 + 48*x**3 + 312*x**2 - 576*x - 100
+    funct = sy.lambdify(x,f,"numpy")
+
+    #find critical points
+    df = sy.diff(f,x)
+    cp = sy.solve(df, x)
+
+    #calculate double derivative at these points
+    ddf = sy.diff(df,x)
+    local = sy.lambdify(x, ddf, "numpy")
+
+    #plug in critical points to calculate maxima and minima
+    maxima = []
+    minima = []
+    for i in cp:
+        if local(i) > 0:
+            minima.append(i)
+        elif local(i) < 0:
+            maxima.append(i)
+
+    domain = np.linspace(-5,5,1000)
+
+    #plot initial polynomial and min and max
+    plt.plot(domain, funct(domain))
+    plt.plot(np.array(minima),funct(np.array(minima)), '.r', label="min")
+    plt.plot(np.array(maxima),funct(np.array(maxima)), '.b', label="max")
+    plt.title("local min and max of polynomial")
+    plt.legend()
+    plt.show()
+
+    return set(minima), set(maxima)
+
+
 
 
 # Problem 7
@@ -89,4 +199,28 @@ def prob7():
     Returns:
         (float): the integral of f over the sphere of radius 2.
     """
-    raise NotImplementedError("Problem 7 Incomplete")
+    #create integrand
+    x,y,z,r,o,t,rad = sy.symbols('x y z r o t rad')
+
+    f = (x**2 + y**2 + z**2)**2
+    h = f.subs({x: r*sy.sin(o)*sy.cos(t), y: r*sy.sin(o)*sy.sin(t), z:r*sy.cos(o)})
+
+    m = sy.Matrix([r*sy.sin(o)*sy.cos(t), r*sy.sin(o)*sy.sin(t), r*sy.cos(o)])
+    J = sy.simplify(m.jacobian([r,t,o]).det())
+    integrand = sy.simplify(h*-J)
+
+    #symbollic radius function
+    rad_f = sy.integrate(integrand, (r,0,rad), (t,0,2*np.pi), (o, 0, np.pi))
+
+    r_lamb = sy.lambdify(rad, rad_f, "numpy")
+    domain = np.linspace(0,3,1000)
+
+    #plot the integral and return when r=2
+    plt.plot(domain, r_lamb(domain))
+    plt.title("Integrand")
+    plt.show()
+
+    return r_lamb(2)
+
+if __name__ == "__main__":
+    print(prob5())
